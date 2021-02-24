8d1ad248afcca44263375324ac7bc3cf3c993037
Abhishek Purohit <abpuro@microsoft.com>
2021-02-25 05:27:30 +0530

    version1

---
+++ precache-manifest.9e935c372106408a8c8a39df2367a615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f409dcd170d2d011e1e7495e2418c60c",
+    "url": "/pcapiexplorer/index.html"
+  },
+  {
+    "revision": "da1d98763ebe05a1cabb",
+    "url": "/pcapiexplorer/static/css/main.cb68ba84.chunk.css"
+  },
+  {
+    "revision": "b718d68f61203c7c1c6a",
+    "url": "/pcapiexplorer/static/js/2.73702e83.chunk.js"
+  },
+  {
+    "revision": "da1d98763ebe05a1cabb",
+    "url": "/pcapiexplorer/static/js/main.f45ee5e6.chunk.js"
+  },
+  {
+    "revision": "ac2a8e07a5c47e727abf",
+    "url": "/pcapiexplorer/static/js/runtime-main.1401919e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pcapiexplorer/static/media/logo.25bf045c.svg"
+  }
+]);
