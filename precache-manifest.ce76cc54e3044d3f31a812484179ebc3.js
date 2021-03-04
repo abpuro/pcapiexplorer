84fb799648331adb0947dd3740a76acf0bca4d76
Abhishek Purohit <abpuro@microsoft.com>
2021-03-05 02:23:36 +0530

    Updated Switch and Home Page

---
+++ precache-manifest.ce76cc54e3044d3f31a812484179ebc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446ca21b1f380576250cd5b9c6f89047",
+    "url": "/pcapiexplorer/index.html"
+  },
+  {
+    "revision": "67599a8c3ade8f16ec4d",
+    "url": "/pcapiexplorer/static/css/main.cb68ba84.chunk.css"
+  },
+  {
+    "revision": "5fcdb4cfe5eb224e012c",
+    "url": "/pcapiexplorer/static/js/2.7f7f6f27.chunk.js"
+  },
+  {
+    "revision": "67599a8c3ade8f16ec4d",
+    "url": "/pcapiexplorer/static/js/main.760d469a.chunk.js"
+  },
+  {
+    "revision": "ac2a8e07a5c47e727abf",
+    "url": "/pcapiexplorer/static/js/runtime-main.1401919e.js"
+  }
+]);
